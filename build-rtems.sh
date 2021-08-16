e2eb04ddfab5d11c4f5b8ce6d968de174f19381d
Marek Slowinski <mslowinski@internships.antmicro.com>
2021-08-16 08:44:13 +0200

    Add rtems-shell application to LEON3 target in Makefile

--- build-rtems.sh
+++ build-rtems.sh
@@ -5,5 +5,8 @@ wget https://www.gaisler.com/anonftp/rcc/bin/linux/sparc-rtems-5-gcc-10.2.0-1.3.
 tar -Jxf sparc-rtems-5-gcc-10.2.0-1.3.0-linux.txz
 ln -s ~/rcc-1.3.0-gcc/ /opt/
 pushd rcc-1.3.0-gcc/src/samples/
+# Add rtems-shell application to LEON3 target
+# as by default it is included only for LEON2
+sed -i.bkp '/LEON3_ALL= \\/a rtems-shell \\' Makefile
 make leon3
 popd
