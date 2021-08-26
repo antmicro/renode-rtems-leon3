fca42826458fda4724a5d4e62d0e79686840cabb
Piotr Zierhoffer <pzierhoffer@antmicro.com>
2021-08-26 16:37:06 +0200

    Simplify the rtems build script

--- build-rtems.sh
+++ build-rtems.sh
@@ -3,7 +3,6 @@ set -euo pipefail
 
 wget --progress=bar https://www.gaisler.com/anonftp/rcc/bin/linux/sparc-rtems-5-gcc-10.2.0-1.3.0-linux.txz
 tar -Jxf sparc-rtems-5-gcc-10.2.0-1.3.0-linux.txz
-ln -s ~/rcc-1.3.0-gcc/ /opt/
 pushd rcc-1.3.0-gcc/src/samples/
 # Add rtems-shell application to LEON3 target
 # as by default it is included only for LEON2
