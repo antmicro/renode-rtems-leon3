e71fc20bb556d3f7c57c9d76e16ea6dd1b167fa4
Piotr Zierhoffer <pzierhoffer@antmicro.com>
2021-08-26 16:52:50 +0200

    Save artifacts

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,3 +4,7 @@ set -e
 ./build-prom.sh
 
 test.sh leon3_rtems.robot
+
+mkdir -p artifacts
+
+cp -r report.html log.html robot_output.xml rcc-1.3.0-gcc/src/samples/bin/leon3 grlib-gpl-2021.2-b4267/software/leon3/prom.bin artifacts
