742785ea4fc5e46bf59a71d35ddb2446ed6f1e53
Piotr Zierhoffer <pzierhoffer@antmicro.com>
2021-08-26 16:28:22 +0200

    Clean up the output of build scripts

--- build-prom.sh
+++ build-prom.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -euo pipefail
 
-wget --no-verbose 'https://www.gaisler.com/anonftp/bcc2/bin/bcc-2.2.0-gcc-linux64.tar.xz'
+wget --progress=bar 'https://www.gaisler.com/anonftp/bcc2/bin/bcc-2.2.0-gcc-linux64.tar.xz'
 tar xf bcc-2.2.0-gcc-linux64.tar.xz
 
-wget --no-verbose 'https://www.gaisler.com/products/grlib/grlib-gpl-2021.2-b4267.tar.gz'
+wget --progress=bar 'https://www.gaisler.com/products/grlib/grlib-gpl-2021.2-b4267.tar.gz'
 tar xf grlib-gpl-2021.2-b4267.tar.gz
 
 export PATH="$PATH:$PWD/bcc-2.2.0-gcc/bin"
--- build-rtems.sh
+++ build-rtems.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 set -euo pipefail
 
-wget --no-verbose https://www.gaisler.com/anonftp/rcc/bin/linux/sparc-rtems-5-gcc-10.2.0-1.3.0-linux.txz
+wget --progress=bar https://www.gaisler.com/anonftp/rcc/bin/linux/sparc-rtems-5-gcc-10.2.0-1.3.0-linux.txz
 tar -Jxf sparc-rtems-5-gcc-10.2.0-1.3.0-linux.txz
 ln -s ~/rcc-1.3.0-gcc/ /opt/
 pushd rcc-1.3.0-gcc/src/samples/
 # Add rtems-shell application to LEON3 target
 # as by default it is included only for LEON2
 sed -i.bkp '/LEON3_ALL= \\/a rtems-shell \\' Makefile
-make leon3
+make -j`nproc` leon3
 popd
