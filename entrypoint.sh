b9b06da65ea97f0784c911540b2f07743ba5e3bd
Piotr Zierhoffer <pzierhoffer@antmicro.com>
2021-09-02 17:22:41 +0200

    Specify artifacts better

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,4 +7,4 @@ test.sh leon3_rtems.robot
 
 mkdir -p artifacts
 
-cp -r report.html log.html robot_output.xml rcc-1.3.0-gcc/src/samples/bin/leon3 grlib-gpl-2021.2-b4267/software/leon3/prom.bin artifacts
+cp -r report.html log.html robot_output.xml rcc-1.3.0-gcc/src/samples/bin/leon3/{rtems-shell,rtems-tlib,rtems-hello,rtems-ttcp} grlib-gpl-2021.2-b4267/software/leon3/prom.bin artifacts
