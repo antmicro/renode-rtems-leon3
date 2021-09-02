ba907c441a1ea9b4cdee1667c600671f4c7ea2a3
Piotr Zierhoffer <pzierhoffer@antmicro.com>
2021-09-02 23:58:33 +0200

    Make entrypoint sh-friendly

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,4 +7,8 @@ test.sh leon3_rtems.robot
 
 mkdir -p artifacts
 
-cp -r report.html log.html robot_output.xml rcc-1.3.0-gcc/src/samples/bin/leon3/{rtems-shell,rtems-tlib,rtems-hello,rtems-ttcp} grlib-gpl-2021.2-b4267/software/leon3/prom.bin artifacts
+for i in rtems-shell rtems-tlib rtems-hello rtems-ttcp
+do
+    cp rcc-1.3.0-gcc/src/samples/bin/leon3/$i artifacts
+done
+cp -r report.html log.html robot_output.xml grlib-gpl-2021.2-b4267/software/leon3/prom.bin artifacts
